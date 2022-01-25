99e3e6f7bc09be1138a0e4d5fddd9dc9d4345920
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2022-01-25 03:57:18 +0100

    bocle

--- src/Home.java
+++ src/Home.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Home {
     public static void main(String[] agrs){
         Scanner scanner=new Scanner(System.in);
+//        //for while do while
+//        //    break continue
+//        for(int i=0;i<10;i++){
+//            if(i==5)
+//                continue;
+//            System.out.println(i);
+//        }
+//        System.out.println("next");
 
     }
+    //if
+    //switch
+    //ternary (condition?traitement(true):traitement(flase))
 }
